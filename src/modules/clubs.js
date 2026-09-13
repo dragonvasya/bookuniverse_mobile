50bf02de415a837f1992ae4425dd2b8bf4d3d513
dragonvasya <avvaskov@gmail.com>
2026-09-14 02:10:07 +0300

    feat: add Shrift logo to mobile

--- src/modules/clubs.js
+++ src/modules/clubs.js
@@ -65,6 +65,7 @@ function getLogoSrc(club) {
         cl26: '/bookz.jpg',          // Bookz Club, Москва
         cl27: '/dubai.jpg',          // Книжный клуб Дубай
         cl28: '/gromko-logo.jpg',    // Громко сказано, Москва
+        cl30: '/shrift-logo.jpg',    // Шрифт, Казань
     };
     return logoMap[club.id] || null;
 }
